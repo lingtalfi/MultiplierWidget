8f6841c5f83981fe764132eae78afdc32622c30a
lingtalfi <lingtalfi@gmail.com>
2018-04-04 10:41:20 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,105 @@
+MultiplierWidget
+===========
+2018-04-04
+
+
+
+A javascript object that can multiply items.
+
+
+
+
+Why?
+==========================
+
+We can use this in forms, for instance when you want the user to be able to select
+multiple products from your database.
+
+
+
+How?
+========
+
+```php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+    <script
+            src="https://code.jquery.com/jquery-3.3.1.min.js"
+            integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
+            crossorigin="anonymous"></script>
+
+    <script src="/libs/multiplier-widget/multiplier-widget.js"></script>
+
+</head>
+
+<body>
+
+
+<form method="post" action="">
+
+
+    <div class="multiplier-gui">
+
+        <div>
+
+
+            <div class="multiplier-items-container">
+
+            </div>
+
+            <div>
+                <select class="multiplier">
+                    <option value="a">a</option>
+                    <option value="b">b</option>
+                    <option value="c">c</option>
+                </select>
+            </div>
+            <button class="multiplier-add-btn">Add</button>
+            <div style="display: none">
+                <div class="multiplier-item-template">
+                    My value is <span class="multiplier-item-value-placeholder"></span>
+                    <button class="multiplier-delete-btn">Delete</button>
+                </div>
+            </div>
+
+        </div>
+
+
+    </div>
+</form>
+
+
+<script>
+    $(document).ready(function () {
+
+
+        var jGui = $(".multiplier-gui");
+        var o = new window.multiplierWidget({
+            gui: jGui
+        });
+
+    });
+</script>
+
+
+</body>
+</html>
+```
+
+
+
+
+
+History Log
+------------------
+        
+- 1.0.0 -- 2018-04-04
+
+    - initial commit
+
+
+
+
---
+++ multiplier-widget.js
@@ -0,0 +1,85 @@
+//----------------------------------------
+// MULTIPLIER WIDGET
+//----------------------------------------
+if ("undefined" === typeof window.multiplierWidget) {
+
+
+    window.multiplierWidget = function (options) {
+
+        var zis = this;
+        this.error = function (msg) {
+            alert("multiplierWidget error: " + msg);
+        };
+
+        this.find = function (cssClass, jGui) {
+            var jTmpEl = jGui.find("." + cssClass);
+            if (jTmpEl.length) {
+                return jTmpEl;
+            }
+            zis.error("item not found with class " + cssClass);
+            throw new Error("Fix errors first...");
+        };
+
+
+        var o = $.extend(true, {
+            gui: null,
+            multiplier: null,
+            multiplierClass: "multiplier",
+            itemsContainerClass: "multiplier-items-container",
+            addBtnClass: "multiplier-add-btn",
+            itemTemplateClass: "multiplier-item-template",
+            itemValuePlaceHolderClass: "multiplier-item-value-placeholder",
+            deleteBtnClass: "multiplier-delete-btn",
+            getCurrentValue: function (jGui, jMultiplier) {
+                return jMultiplier.val();
+            },
+        }, options);
+
+
+        var jGui, jMultiplier, jItemsContainer, jAddBtn, jItemTemplate;
+
+
+        if (null !== o.gui) {
+            jGui = o.gui;
+
+
+            if (null === o.multiplier) {
+                o.multiplier = jGui.find("." + o.multiplierClass);
+            }
+
+            if (o.multiplier instanceof jQuery) {
+
+                //----------------------------------------
+                // INIT
+                //----------------------------------------
+                jMultiplier = o.multiplier;
+                jItemsContainer = this.find(o.itemsContainerClass, jGui);
+                jAddBtn = this.find(o.addBtnClass, jGui);
+                jItemTemplate = this.find(o.itemTemplateClass, jGui);
+
+
+                jAddBtn.on('click', function () {
+                    var value = o.getCurrentValue(jGui, jMultiplier);
+                    var jClonedItem = jItemTemplate.clone();
+                    jItemsContainer.append(jClonedItem);
+                    jClonedItem.find('.' + o.itemValuePlaceHolderClass).html(value);
+                    return false;
+                });
+
+                jGui.on('click.multiplierWidget', '.' + o.deleteBtnClass, function () {
+                    $(this).closest("." + o.itemTemplateClass).remove();
+                    return false;
+                });
+
+
+            }
+            else {
+                this.error('cannot find the multiplier element, check your config or your html');
+            }
+        }
+        else {
+            this.error('bad configuration: host required');
+        }
+    };
+
+}
